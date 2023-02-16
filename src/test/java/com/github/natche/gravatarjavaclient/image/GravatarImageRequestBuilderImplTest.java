dd1869dbd8f0b84a6b89ca2c235b8ba6882a1911
Nathan <nathan.vincent.2.718@gmail.com>
2023-02-15 22:06:30 -0600

    test

--- src/test/java/com/github/natche/gravatarjavaclient/image/GravatarImageRequestBuilderImplTest.java
+++ src/test/java/com/github/natche/gravatarjavaclient/image/GravatarImageRequestBuilderImplTest.java
@@ -178,7 +178,7 @@ class GravatarImageRequestBuilderImplTest {
         assertNotEquals(equalImpl1, new Object());
 
         GravatarImageRequestBuilderImpl truthBuilder = new GravatarImageRequestBuilderImpl("n@email.com");
-        assertEquals(truthBuilder, new GravatarImageRequestBuilderImpl("n@email.com")
+        assertNotEquals(truthBuilder, new GravatarImageRequestBuilderImpl("n@email.com")
                 .setShouldAppendJpgSuffix(false));
         assertNotEquals(truthBuilder, new GravatarImageRequestBuilderImpl("something@email.com")
                 .setShouldAppendJpgSuffix(true));
