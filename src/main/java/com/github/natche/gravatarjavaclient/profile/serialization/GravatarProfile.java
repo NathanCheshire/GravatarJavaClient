85d65cfb31b450e863b1f5af34a275567fd7f37e
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2024-09-10 11:44:17 -0500

    adding more structgure to gravatar profile

--- src/main/java/com/github/natche/gravatarjavaclient/profile/serialization/GravatarProfile.java
+++ src/main/java/com/github/natche/gravatarjavaclient/profile/serialization/GravatarProfile.java
@@ -1,10 +1,13 @@
 package com.github.natche.gravatarjavaclient.profile.serialization;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.errorprone.annotations.Immutable;
 import com.google.gson.annotations.SerializedName;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * The results of a Gravatar profile request.
@@ -13,144 +16,223 @@ import java.time.Instant;
 @Immutable
 public final class GravatarProfile {
     /**
-     * The SHA256 hash of the user’s primary email address.
+     * The SHA256 hash of the user's primary email address.
      */
-    private String hash;
+    private final String hash;
 
     /**
-     * The user’s display name that appears on their profile.
+     * The user's display name that appears on their profile.
      */
     @SerializedName("display_name")
-    private String displayName;
+    private final String displayName;
 
     /**
-     * The full URL to the user’s Gravatar profile.
+     * The full URL to the user's Gravatar profile.
      */
     @SerializedName("profile_url")
-    private String profileUrl;
+    private final String profileUrl;
 
     /**
-     * The URL to the user’s avatar image, if set.
+     * The URL to the user's avatar image, if set.
      */
     @SerializedName("avatar_url")
-    private String avatarUrl;
+    private final String avatarUrl;
 
     /**
-     * Alternative text describing the user’s avatar.
+     * Alternative text describing the user's avatar.
      */
     @SerializedName("avatar_alt_text")
-    private String avatarAltText;
+    private final String avatarAltText;
 
     /**
-     * The user’s geographical location.
+     * The user's geographical location.
      */
-    private String location;
+    private final String location;
 
     /**
      * A short biography or description about the user found on their profile.
      */
-    private String description;
+    private final String description;
 
     /**
-     * The user’s current job title.
+     * The user's current job title.
      */
     @SerializedName("job_title")
-    private String jobTitle;
+    private final String jobTitle;
 
     /**
      * The name of the company where the user is employed.
      */
-    private String company;
+    private final String company;
 
     /**
      * A list of verified accounts the user has added to their profile.
      */
     @SerializedName("verified_accounts")
-    private ImmutableList<GravatarProfileVerifiedAccount> verifiedAccounts;
+    private final ImmutableList<GravatarProfileVerifiedAccount> verifiedAccounts;
 
     /**
-     * A phonetic guide to pronouncing the user’s name.
+     * A phonetic guide to pronouncing the user's name.
      */
-    private String pronunciation;
+    private final String pronunciation;
 
     /**
      * The pronouns the user prefers to use.
      */
-    private String pronouns;
+    private final String pronouns;
 
     /**
      * The timezone the user has set.
      */
-    private String timezone;
+    private final String timezone;
 
     /**
      * The languages the user has selected on their profile.
      */
-    private ImmutableList<GravatarProfileLanguage> languages;
+    private final ImmutableList<GravatarProfileLanguage> languages;
 
     /**
      * The user's first name.
      */
     @SerializedName("first_name")
-    private String firstName;
+    private final String firstName;
 
     /**
      * The user's last name.
      */
     @SerializedName("last_name")
-    private String lastName;
+    private final String lastName;
 
     /**
      * Whether this account is marked as an organization account;
      */
     @SerializedName("is_organization")
-    private boolean isOrganization;
+    private final boolean isOrganization;
 
     /**
      * The links the user has displayed on their profile.
      */
-    private ImmutableList<GravatarProfileUrl> links;
+    private final ImmutableList<GravatarProfileUrl> links;
 
     /**
      * The interests the user has displayed on their profile.
      */
-    private ImmutableList<GravatarProfileInterest> interests;
+    private final ImmutableList<GravatarProfileInterest> interests;
 
     /**
      * The payments objects containing the payment data the user has displayed on their account.
      */
-    private GravatarProfilePayments payments;
+    private final GravatarProfilePayments payments;
 
     /**
      * The contact info object containing the contact information the user has displayed on their account
      */
     @SerializedName("contact_info")
-    private GravatarProfileContactInfo contactInfo;
+    private final GravatarProfileContactInfo contactInfo;
 
     /**
      * The gallery images the user has displayed on their account.
      */
-    private ImmutableList<GravatarProfileGalleryImage> gallery;
+    private final ImmutableList<GravatarProfileGalleryImage> gallery;
 
     /**
      * The number of verified accounts associated with this user.
      */
     @SerializedName("number_verified_accounts")
-    private int numberVerifiedAccounts;
+    private final int numberVerifiedAccounts;
 
     /**
      * The last time this account was edited at.
      */
     @SerializedName("last_profile_edit")
-    private String lastProfileEdit;
+    private final String lastProfileEdit;
 
     /**
      * The date this account was registered at.
      */
     @SerializedName("registration_date")
-    private String registrationDate;
+    private final String registrationDate;
 
-    // todo constructor.
+    /**
+     * Constructs a new GravatarProfile.
+     *
+     * @param hash                   the SHA256 hash of the user's primary email address
+     * @param displayName            the user's display name
+     * @param profileUrl             the full URL to the user's Gravatar profile
+     * @param avatarUrl              the URL to the user's avatar image
+     * @param avatarAltText          alternative text describing the user's avatar
+     * @param location               the user's geographical location
+     * @param description            a short biography or description about the user
+     * @param jobTitle               the user's current job title
+     * @param company                the name of the company where the user is employed
+     * @param verifiedAccounts       a list of verified accounts the user has added to their profile
+     * @param pronunciation          a phonetic guide to pronouncing the user's name
+     * @param pronouns               the pronouns the user prefers to use
+     * @param timezone               the timezone the user has set
+     * @param languages              the languages the user has selected on their profile
+     * @param firstName              the user's first name
+     * @param lastName               the user's last name
+     * @param isOrganization         whether this account is marked as an organization account
+     * @param links                  the links the user has displayed on their profile
+     * @param interests              the interests the user has displayed on their profile
+     * @param payments               the payments objects containing the payment data
+     * @param contactInfo            the contact info object containing the contact information
+     * @param gallery                the gallery images the user has displayed on their account
+     * @param numberVerifiedAccounts the number of verified accounts associated with this user
+     * @param lastProfileEdit        the last time this account was edited
+     * @param registrationDate       the date this account was registered
+     */
+    public GravatarProfile(String hash,
+                           String displayName,
+                           String profileUrl,
+                           String avatarUrl,
+                           String avatarAltText,
+                           String location,
+                           String description,
+                           String jobTitle,
+                           String company,
+                           List<GravatarProfileVerifiedAccount> verifiedAccounts,
+                           String pronunciation,
+                           String pronouns,
+                           String timezone,
+                           List<GravatarProfileLanguage> languages,
+                           String firstName,
+                           String lastName,
+                           boolean isOrganization,
+                           List<GravatarProfileUrl> links,
+                           List<GravatarProfileInterest> interests,
+                           GravatarProfilePayments payments,
+                           GravatarProfileContactInfo contactInfo,
+                           List<GravatarProfileGalleryImage> gallery,
+                           int numberVerifiedAccounts,
+                           String lastProfileEdit,
+                           String registrationDate) {
+        this.hash = Preconditions.checkNotNull(hash);
+        this.displayName = displayName;
+        this.profileUrl = Preconditions.checkNotNull(profileUrl);
+        this.avatarUrl = avatarUrl;
+        this.avatarAltText = avatarAltText;
+        this.location = location;
+        this.description = description;
+        this.jobTitle = jobTitle;
+        this.company = company;
+        this.verifiedAccounts = verifiedAccounts != null ? ImmutableList.copyOf(verifiedAccounts) : ImmutableList.of();
+        this.pronunciation = pronunciation;
+        this.pronouns = pronouns;
+        this.timezone = timezone;
+        this.languages = languages != null ? ImmutableList.copyOf(languages) : ImmutableList.of();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.isOrganization = isOrganization;
+        this.links = links != null ? ImmutableList.copyOf(links) : ImmutableList.of();
+        this.interests = interests != null ? ImmutableList.copyOf(interests) : ImmutableList.of();
+        this.payments = payments;
+        this.contactInfo = contactInfo;
+        this.gallery = gallery != null ? ImmutableList.copyOf(gallery) : ImmutableList.of();
+        this.numberVerifiedAccounts = numberVerifiedAccounts;
+        this.lastProfileEdit = lastProfileEdit;
+        this.registrationDate = registrationDate;
+    }
 
     /**
      * Returns the SHA256 hash of the user’s primary email address.
@@ -376,5 +458,114 @@ public final class GravatarProfile {
      */
     public Instant getRegistrationDate() {
         return Instant.parse(registrationDate);
+    }
+
+    /**
+     * Returns whether the provided object is equal to this.
+     *
+     * @param o the other object
+     * @return whether the provided object is equal to this
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof GravatarProfile that)) return false;
+
+        return isOrganization == that.isOrganization
+                && numberVerifiedAccounts == that.numberVerifiedAccounts
+                && Objects.equals(hash, that.hash)
+                && Objects.equals(displayName, that.displayName)
+                && Objects.equals(profileUrl, that.profileUrl)
+                && Objects.equals(avatarUrl, that.avatarUrl)
+                && Objects.equals(avatarAltText, that.avatarAltText)
+                && Objects.equals(location, that.location)
+                && Objects.equals(description, that.description)
+                && Objects.equals(jobTitle, that.jobTitle)
+                && Objects.equals(company, that.company)
+                && Objects.equals(verifiedAccounts, that.verifiedAccounts)
+                && Objects.equals(pronunciation, that.pronunciation)
+                && Objects.equals(pronouns, that.pronouns)
+                && Objects.equals(timezone, that.timezone)
+                && Objects.equals(languages, that.languages)
+                && Objects.equals(firstName, that.firstName)
+                && Objects.equals(lastName, that.lastName)
+                && Objects.equals(links, that.links)
+                && Objects.equals(interests, that.interests)
+                && Objects.equals(payments, that.payments)
+                && Objects.equals(contactInfo, that.contactInfo)
+                && Objects.equals(gallery, that.gallery)
+                && Objects.equals(lastProfileEdit, that.lastProfileEdit)
+                && Objects.equals(registrationDate, that.registrationDate);
+    }
+
+    /**
+     * Returns a hashcode for this object.
+     *
+     * @return a hashcode for this object
+     */
+    @Override
+    public int hashCode() {
+        int ret = hash.hashCode();
+        ret = 31 * ret + (displayName != null ? displayName.hashCode() : 0);
+        ret = 31 * ret + profileUrl.hashCode();
+        ret = 31 * ret + (avatarUrl != null ? avatarUrl.hashCode() : 0);
+        ret = 31 * ret + (avatarAltText != null ? avatarAltText.hashCode() : 0);
+        ret = 31 * ret + (location != null ? location.hashCode() : 0);
+        ret = 31 * ret + (description != null ? description.hashCode() : 0);
+        ret = 31 * ret + (jobTitle != null ? jobTitle.hashCode() : 0);
+        ret = 31 * ret + (company != null ? company.hashCode() : 0);
+        ret = 31 * ret + verifiedAccounts.hashCode();
+        ret = 31 * ret + (pronunciation != null ? pronunciation.hashCode() : 0);
+        ret = 31 * ret + (pronouns != null ? pronouns.hashCode() : 0);
+        ret = 31 * ret + (timezone != null ? timezone.hashCode() : 0);
+        ret = 31 * ret + languages.hashCode();
+        ret = 31 * ret + (firstName != null ? firstName.hashCode() : 0);
+        ret = 31 * ret + (lastName != null ? lastName.hashCode() : 0);
+        ret = 31 * ret + (isOrganization ? 1 : 0);
+        ret = 31 * ret + links.hashCode();
+        ret = 31 * ret + interests.hashCode();
+        ret = 31 * ret + (payments != null ? payments.hashCode() : 0);
+        ret = 31 * ret + (contactInfo != null ? contactInfo.hashCode() : 0);
+        ret = 31 * ret + gallery.hashCode();
+        ret = 31 * ret + numberVerifiedAccounts;
+        ret = 31 * ret + (lastProfileEdit != null ? lastProfileEdit.hashCode() : 0);
+        ret = 31 * ret + (registrationDate != null ? registrationDate.hashCode() : 0);
+        return ret;
+    }
+
+    /**
+     * Returns a string representation of this object.
+     *
+     * @return a string representation of this object
+     */
+    @Override
+    public String toString() {
+        return "GravatarProfile{"
+                + "hash=\"" + hash + "\""
+                + ", displayName=\"" + displayName + "\""
+                + ", profileUrl=\"" + profileUrl + "\""
+                + ", avatarUrl=\"" + avatarUrl + "\""
+                + ", avatarAltText=\"" + avatarAltText + "\""
+                + ", location=\"" + location + "\""
+                + ", description=\"" + description + "\""
+                + ", jobTitle=\"" + jobTitle + "\""
+                + ", company=\"" + company + "\""
+                + ", verifiedAccounts=" + verifiedAccounts
+                + ", pronunciation=\"" + pronunciation + "\""
+                + ", pronouns=\"" + pronouns + "\""
+                + ", timezone=\"" + timezone + "\""
+                + ", languages=" + languages
+                + ", firstName=\"" + firstName + "\""
+                + ", lastName=\"" + lastName + "\""
+                + ", isOrganization=" + isOrganization
+                + ", links=" + links
+                + ", interests=" + interests
+                + ", payments=" + payments
+                + ", contactInfo=" + contactInfo
+                + ", gallery=" + gallery
+                + ", numberVerifiedAccounts=" + numberVerifiedAccounts
+                + ", lastProfileEdit=\"" + lastProfileEdit + "\""
+                + ", registrationDate=\"" + registrationDate + "\""
+                + "}";
     }
 }
