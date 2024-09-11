d0879ba354b314eaae0b21ef3df72754ea4d1859
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2024-09-11 18:53:52 -0500

    add request handler test

--- src/test/kotlin/com/github/natche/gravatarjavaclient/profile/GravatarProfileRequestHandlerTest.kt
+++ src/test/kotlin/com/github/natche/gravatarjavaclient/profile/GravatarProfileRequestHandlerTest.kt
@@ -1,9 +1,11 @@
 package com.github.natche.gravatarjavaclient.profile
 
 import com.github.natche.gravatarjavaclient.exceptions.GravatarJavaClientException
+import com.google.common.base.Preconditions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
 
 /**
  * Tests for the [GravatarProfileRequestHandler].
@@ -19,5 +21,19 @@ class GravatarProfileRequestHandlerTest {
         }
 
         assertEquals("API error: Profile not found", exception.message)
+    }
+
+    /**
+     * Tests for the [Preconditions] on the getProfile method.
+     */
+    @Test
+    fun testPreconditions() {
+        assertThrows(NullPointerException::class.java)
+        { GravatarProfileRequestHandler.INSTANCE.getProfile(ByteArray(0), null) }
+        assertThrows(IllegalArgumentException::class.java)
+        { GravatarProfileRequestHandler.INSTANCE.getProfile(ByteArray(0), "") }
+        assertThrows(IllegalArgumentException::class.java)
+        { GravatarProfileRequestHandler.INSTANCE.getProfile(ByteArray(0), "  ") }
+
     }
 }
