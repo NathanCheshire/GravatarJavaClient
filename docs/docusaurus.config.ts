1449e2d93c5311449e81d2a87d41eb19560eed5e
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2025-10-31 19:50:08 -0500

    add docusaurus start

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,72 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "GravatarJavaClient",
+  tagline: "A JVM client for Gravatar Avatar and Profile APIs",
+  favicon: "img/favicon.ico",
+
+  future: {
+    v4: true,
+  },
+
+  url: "https://your-docusaurus-site.example.com",
+  baseUrl: "/GravatarJavaClient/",
+  projectName: "GravatarJavaClient",
+  onBrokenLinks: "throw",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: "GravatarJavaClient",
+      logo: {
+        alt: "GravatarJavaClient Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "docsSidebar",
+          position: "left",
+          label: "Documentation",
+        },
+        {
+          href: "https://github.com/nathancheshire/GravatarJavaClient",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "light",
+      copyright: `Copyright © 2023 - 2025 GravatarJavaClient. Docs built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.oneLight,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
